25ca6a557b367e92d32d9b532d58dbc86a4e10e8
Malte_Nordlöf <mustgames.pc@gmail.com>
2020-10-05 10:42:44 +0200

    v1.0

--- Prov1App/Program.cs
+++ Prov1App/Program.cs
@@ -7,55 +7,16 @@ namespace Prov1App
 
         static void Main(string[] args)
         {
-            int shipLocation = 6;
-            int Miss;
-            int Bullets = 10;
 
-            string playerInput;
-            int playerGuess;
-            Boolean enemyShipAlive = true;
             Start();
-
-            while (Bullets > 0)
-            {
-                if (enemyShipAlive == true)
-                {
-                    playerInput = Console.ReadLine();
-                    Console.Clear();
-                    playerGuess = int.Parse(playerInput);
-                    Miss = shipLocation - playerGuess;
-
-                    if (playerGuess > 0 && playerGuess < 11)
-                    {
-                        if (playerGuess == shipLocation)
-                        {
-                            enemyShipAlive = false;
-                            Bullets = 1;
-                        }
-                        else
-                        {
-                            Console.WriteLine("We missed by" + Miss);
-                            Bullets--;
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Thats not a valid cordenate captain, try again");
-                    }
-                }
-                else
-                {
-                    Win();
-                }
-            }
         }
         static void Start()
         {
 
-            Console.WriteLine("Welcome to battle ships, lets sink the enemy ship! Press a key if you are ready");
-            Console.ReadKey();
+            Console.WriteLine("Welcome to battle ships, lets sink the enemy ship!");
             Console.WriteLine("There is a big enemy aircraft carrier infront of us on our raidar, but we don't have its exact location. What do we do Captain?");
             Console.WriteLine("Choose where to shot (by guessing between 1-10)");
+            PlayGame();
         }
         static void Win()
         {
@@ -65,7 +26,49 @@ namespace Prov1App
             Console.WriteLine("You Sinked the ship and won Congratualtions");
             Console.WriteLine("Play Again? (y) yes (n) for no");
             playAgainInput = Console.ReadLine();
+            if (playAgainInput == "y")
+            {
+                Start();
+            }
 
+        }
+        static void PlayGame()
+        {
+            int shipLocation = 6;
+            int Miss;
+
+            string playerInput;
+            int playerGuess = 0;
+            Boolean enemyShipAlive = true;
+
+            while (enemyShipAlive == true)
+            {
+                playerInput = Console.ReadLine();
+                Console.Clear();
+                try
+                {
+                    playerGuess = int.Parse(playerInput);
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("Thats not a valid cordenate captain, try again");
+                }
+                Miss = shipLocation - playerGuess;
+
+                if (playerGuess > 0 && playerGuess < 11)
+                {
+                    if (playerGuess == shipLocation)
+                    {
+                        enemyShipAlive = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("We missed by: " + Math.Abs(Miss));
+                        Console.WriteLine("Shoot another shot! (Guess between 1-10)");
+                    }
+                }
+            }
+            Win();
         }
     }
 }
